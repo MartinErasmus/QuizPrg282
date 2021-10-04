727e3da501eba140b86625db5c00e26b20e3eb93
MartinErasmus <martinerasmus1999@gmail.com>
2021-10-04 16:39:03 +0200

    ListBox Code

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace git_activity_4
 {
@@ -13,6 +14,18 @@ namespace git_activity_4
         public Form1()
         {
             InitializeComponent();
+        }
+              private void button1_Click(object sender, EventArgs e)
+        {
+                string path =@".\Untitled.txt";
+                if(File.Exists(path)){
+                    using(StreamReader reader= new StreamReader(path)){
+                        string text;
+                        while((text = reader.ReadLine()) != null){
+                             listBox1.Items.Add(text);
+                        }
+                    }
+                }
         }
     }
 }
